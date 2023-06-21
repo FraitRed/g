3cfd22592738bb22f49cb01020bca6afa1420580
FraitRed <136161358+FraitRed@users.noreply.github.com>
2023-06-21 12:27:58 +0300

    Update install.ps1

--- wallpaper/install.ps1
+++ wallpaper/install.ps1
@@ -1,13 +1,12 @@
 #1
 Set-Location $env:Temp
-Invoke-WebRequest https://wallpapersmug.com/download/2048x1152/2d07b6/cute-anime-girl-cake.jpg -O Anime.jpg
+Invoke-WebRequest https://w.forfun.com/fetch/9f/9f6e10f93c578b57172b33a9b0545594.jpeg -O Anime.jpg
 Invoke-WebRequest https://raw.githubusercontent.com/FraitRed/g/main/wallpaper/script.ps1 -O script.ps1
-Invoke-WebRequest https://www.dropbox.com/s/8y9rcp0z8qlowxd/Connection.wav?dl=0 -O Connection.wav
-Invoke-WebRequest https://www.dropbox.com/s/heghwxt0nf504xo/Disconnection.wav?dl=0 -O Disconnection.wav
-Invoke-WebRequest https://www.dropbox.com/s/862izwnhrrr0zaq/genshin.ico?dl=0 -O genshin.ico
-Invoke-WebRequest https://www.dropbox.com/s/z1j8bbfnuy0nl37/Naruto.wav?dl=0 -O Naruto.wav
-Invoke-WebRequest https://www.dropbox.com/s/hwhn6q1t6t18jl8/xui2.cur?dl=0 -O xui2.cur
-Invoke-WebRequest https://www.dropbox.com/s/u5vu3i4xv2ioqnw/Anime.theme?dl=0 -O Anime.theme
+Invoke-WebRequest https://raw.githubusercontent.com/FraitRed/g/main/wallpaper/Connection.wav -O Connection.wav
+Invoke-WebRequest https://raw.githubusercontent.com/FraitRed/g/main/wallpaper/Disconnection.wav -O Disconnection.wav
+Invoke-WebRequest https://raw.githubusercontent.com/FraitRed/g/main/wallpaper/genshin.ico -O genshin.ico
+Invoke-WebRequest https://raw.githubusercontent.com/FraitRed/g/main/wallpaper/xui2.cur -O xui2.cur
+Invoke-WebRequest https://raw.githubusercontent.com/FraitRed/g/main/wallpaper/Anime.theme -O Anime.theme
 
 #2
 Set-Location $env:AppData\Microsoft\Windows\"Start Menu"\Programs\Startup
